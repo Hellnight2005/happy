27d0f90dc90f9a66ab9a8298b994a955f0b62c24
Hellnight2005 <abhijeet2005shinde@gmail.com>
2025-08-22 20:03:02 +0530

    this is second test of the pyhton file

---
+++ test1.py
@@ -0,0 +1,11 @@
+def fibonacci_series(n):
+    series = []
+    a, b = 0, 1
+    for _ in range(n):
+        series.append(a)
+        a, b = b, a + b
+    return series
+
+# Example usage:
+num_terms = 10
+print(fibonacci_series(num_terms))
